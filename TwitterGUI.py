976b36b1db6f4fed26446a45f42565acb5a27d40
Achraf-Ben <achraf.ben.a@outlook.com>
2017-11-07 13:52:16 +0100

    Weer API

--- TwitterGUI.py
+++ TwitterGUI.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageTk
 import sqlite3
 import time
 import datetime
+import twitterAPI
 
 def weersvoorspelling():
     '''Returns weather forecast using the Darsky API'''
@@ -18,7 +19,7 @@ def weersvoorspelling():
 
 # Tkinter Window
 class Window(Frame):
-
+    twitterAPI_class = twitterAPI.Twitter()
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
@@ -28,34 +29,48 @@ class Window(Frame):
         '''Creation of init_window'''
         self.master.title("NS - Twitter & Weer") #Titel of the master widget
         self.pack(fill=BOTH, expand=1)  # Allow full space of root
+        self.configure(background="DeepSkyBlue2")
 
         # NS Logo
         load = Image.open("ns_logo.png")
         render = ImageTk.PhotoImage(load)
-        img = Label(self, image=render)
+        img = Label(self, image=render, bg="DeepSkyBlue2")
         img.image = render
         img.place(x=0, y=0)
 
         # Intro tekst
-        intro = Label(self, text="Welkom bij de NS Twitter Zuil. \n")
+        intro = Label(self, text="Welkom bij de NS Twitter Zuil. \n", bg="DeepSkyBlue2", height=2, padx=15, font=("Helvetica", 16), fg="white")
         intro.pack()
 
-        # Huidige weersomstandigheden
-        tekst = Label(self, text="Huidige weersomstandigheden in Utrecht:\n " + weersvoorspelling())
-        tekst.pack()
-
         # Show Tweets
-        self.label = Label(self, text='Meest recente tweets:', height=2, padx=15, font=("Helvetica", 16), fg="white")
-        self.label.configure(background='DeepSkyBlue2')
-        self.label.pack()
+        listText = self.twitterAPI_class.getTweets()
+        if listText != []:
+            self.label = Label(self, text='Meest recente tweets:', height=2, padx=15, font=("Helvetica", 16),
+                               fg="white")
+            self.label.configure(background='DeepSkyBlue2')
+            self.label.pack()
+
+            for text in listText:
+                self.label = Label(self, text=text, height=2, padx=15,
+                                   font=("Helvetica", 14), fg="white")
+                self.label.configure(background='DeepSkyBlue2')
+                self.label.pack()
+        else:
+            # Huidige weersomstandigheden
+            tekst = Label(self, text="Huidige weersomstandigheden in Utrecht:\n " + weersvoorspelling(),
+                          bg="DeepSkyBlue2", height=2, padx=15, font=("Helvetica", 16), fg="white")
+            tekst.pack()
 
 
 root = Tk() #Creates root window
 root.geometry("1366x768") # Window size
+root.configure(background='DeepSkyBlue2')
 app = Window(root) #Create the instance
 root.mainloop() # Start mainloop to show it
 
 # NS Kleur geel = #FFC917
+
+
 
 """"
 Bronnen:
