9a52875544062a7bcc1fbcc31ac552c4f54302b9
IcefireSijtsema <31849221+IcefireSijtsema@users.noreply.github.com>
2017-11-07 14:38:37 +0100

    Update AdminGUI.py

--- AdminGUI.py
+++ AdminGUI.py
@@ -4,43 +4,36 @@ import sqlite3
 class Window(Frame):
 
 
-    def __init__(self, master = None):                      #main window
+    def __init__(self, master = None):  #Creates main window
+        """ Creates main window """
         Frame.__init__(self, master)
-
         self.master = master
-
         self.init_window()
 
     def init_window(self):
-
-        self.master.title('Admin GUI')
-
+        """ Initializes the layout of the GUI """
+        self.master.title('Admin GUI') #Gives a title to the window
         self.pack(fill=BOTH, expand = 1)
-        self.configure(bg = 'DeepSkyBlue2')
+        self.configure(bg = 'DeepSkyBlue2') #Changes the background colour
 
-        frame = Frame(root)
+        #Creates the textframe where the tweets appear
+        text = Label(self, bg="DeepSkyBlue2", fg="white" ,text="Sample Tweet", font=("Helvetica", 14)).pack(side=TOP)
 
-        text = Label(self, bg="DeepSkyBlue2", fg="white" ,text="Wat vind jij van het hedendaags feminisme?", font=("Helvetica", 14)).pack(side=TOP)
-
+        #Creates the Accept button
         Accept = Button(self, font=("Helvetica",16), fg="Green2", background='white', text='Accept Tweet')
-
         Accept.pack(side="left", expand=True, pady=20)
 
+        #Creates the Reject button
         Reject = Button(self,font=("Helvetica",16), fg="red", background='white', text = 'Reject Tweet')
-
         Reject.pack(side="right", expand=True, pady=20)
-
-        #frame.config(bg="white")
-        #frame.pack(fill=BOTH, expand=True)
 
 
 def exit_window(self):
-        exit()
+    """" Executes the code above """
+    exit()
 
 root = Tk()
-
-root.configure(background="DeepSkyBlue2", padx=20, pady=50)
-root.geometry('400x300')
+root.geometry('500x200') #Makes the window 500px by 200px
 app = Window(root)
 
 root.mainloop()
