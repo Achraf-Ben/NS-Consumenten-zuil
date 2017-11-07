d52a56269b5875a05801c936468a9e23a52d4801
Achraf-Ben <achraf.ben.a@outlook.com>
2017-11-07 13:07:22 +0100

    Weer API

--- Code snippets/TwitterGUI.py
+++ Code snippets/TwitterGUI.py
@@ -1,22 +1,24 @@
 import forecastio
 from tkinter import *
 from PIL import Image, ImageTk
-import sqlite3
-import time
-import datetime
 
-def weersvoorspelling():
-    '''Returns weather forecast using the Darsky API'''
-    api_key = "dca7849ad2985d67e63bdce010c191ee"
-    # Utrecht
-    latitude = 52.0884851
-    longitude = 5.1180588
-    forecast = forecastio.load_forecast(api_key, latitude, longitude)
-    byHour = forecast.daily()
-    weerZin = byHour.summary
-    return weerZin
+import threading
+import tweepy
+import json
+from queue import Queue
 
-# Tkinter Window
+
+#Darsky / Forecastio
+api_key = "dca7849ad2985d67e63bdce010c191ee"
+# Utrecht
+latitude = 52.0884851
+longitude = 5.1180588
+forecast = forecastio.load_forecast(api_key, latitude, longitude)
+byHour = forecast.daily()
+weerZin = byHour.summary
+weerWoord = byHour.icon
+
+# Tkinter Windows
 class Window(Frame):
 
     def __init__(self, master=None):
@@ -27,7 +29,7 @@ class Window(Frame):
     def init_window(self):
         '''Creation of init_window'''
         self.master.title("NS - Twitter & Weer") #Titel of the master widget
-        #self.pack(fill=BOTH, expand=1)  # Allow full space of root
+        self.pack(fill=BOTH, expand=1)  # Allow full space of root
 
         # NS Logo
         load = Image.open("ns_logo.png")
@@ -38,16 +40,19 @@ class Window(Frame):
 
         # Intro tekst
         intro = Label(self, text="Welkom bij de NS Twitter Zuil. \n")
-        intro.grid(row=0)
+        intro.pack()
 
         # Huidige weersomstandigheden
-        tekst = Label(self, text="Huidige weersomstandigheden in Utrecht:\n " + weersvoorspelling())
+        tekst = Label(self, text="Huidige weersomstandigheden in Utrecht:\n " + weerZin)
+        tekst.pack()
+
+        # Huidige weersomstandigheden - 1 woord
+        tekst = Label(self, text=weerWoord)
         tekst.pack()
 
         # Show Tweets
         tweets = Label(self, text="Tweets")
         tweets.pack()
-
 
 root = Tk() #Creates root window
 root.geometry("1366x768") # Window size
@@ -59,10 +64,9 @@ root.mainloop() # Start mainloop to show it
 """"
 Bronnen:
 https://pypi.python.org/pypi/python-forecastio/
-http://tweepy.readthedocs.io/en/v3.5.0/api.html
 https://pythonprogramming.net/python-3-tkinter-basics-tutorial/
 https://pythonprogramming.net/tkinter-python-3-tutorial-adding-buttons/?completed=/python-3-tkinter-basics-tutorial/
-https://pythonprogramming.net/tkinter-adding-text-images/
-https://pythonprogramming.net/twitter-api-streaming-tweets-python-tutorial/?completed=/mysql-live-database-example-streaming-data/
-https://stackoverflow.com/questions/36590476/taking-data-from-a-database-and-putting-into-a-table-in-a-gui
+
+https://github.com/geduldig/TwitterAPI
+http://docs.python-requests.org/en/latest/user/quickstart/
 """
