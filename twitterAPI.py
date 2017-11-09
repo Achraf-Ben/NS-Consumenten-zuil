847f8c40615a227719b06fe144eb15bdec4b45ce
Achraf-Ben <achraf.ben.a@outlook.com>
2017-11-09 10:19:15 +0100

    Updated version number, code reviewed by Achraf.
    - Added try and except.
    - Verified DOC strings.
    - Removed empty lines.
    - Removed print statements.
    - Added two empty lines after functions.

--- twitterAPI.py
+++ twitterAPI.py
@@ -1,7 +1,6 @@
 import tweepy
 import datetime
 
-
 class Twitter():
 
     accessToken = "927464600936828929-g8R4LsDaKbcXSxqW59khKZlTlJ1QM5T"
@@ -16,35 +15,36 @@ class Twitter():
 
 
     def updateTwitter(self, text):
-        '''send de tweet met de bijbehorende tekst'''
+        '''send de tweet met de bijbehorende tekst.'''
         self.api.update_status(text)
 
 
     def getTweets(self):
-        '''haalt de tweets op die binnen 2 uur zijn gepost en stuurt een lijst van de tweets terug'''
-        new_tweets = self.api.user_timeline(page_limit = 20)
-        print(new_tweets)
-        listTexts = []
-        #houd rekening met de tijdzone
-        timezone = 3600
-        # tweets die binnen 2 uur gepost zijn mogen worden opgenomen in de lijst
-        timeTweet = 7200
-        totalCOmpareTime = timezone + timeTweet
+        '''haalt de tweets op die binnen 2 uur zijn gepost en stuurt een lijst van de tweets terug.'''
+        try:
+            new_tweets = self.api.user_timeline(page_limit = 20)
+            listTexts = []
+            #houd rekening met de tijdzone
+            timezone = 3600
+            # tweets die binnen 2 uur gepost zijn mogen worden opgenomen in de lijst
+            timeTweet = 7200
+            totalCOmpareTime = timezone + timeTweet
 
-        numCount = 0
-        for tweet in new_tweets:
-            print(tweet.created_at)
-            if (datetime.datetime.now() - tweet.created_at).total_seconds() < totalCOmpareTime:
-                numCount = numCount + 1
-                text = tweet.text
-                date = tweet.created_at
-                listTexts.append(text)
-                print(text, date)
-                if numCount >= 4:
+            numCount = 0
+            for tweet in new_tweets:
+                if (datetime.datetime.now() - tweet.created_at).total_seconds() < totalCOmpareTime:
+                    numCount = numCount + 1
+                    text = tweet.text
+                    date = tweet.created_at
+                    listTexts.append(text)
+                    if numCount >= 4:
+                        break
+                else:
+                    # de tweets zijn op volgorde van datum, dus de rest van de tweets zijn ouder en hoeven ook niet bekeken te worden
                     break
-            else:
-                # de tweets zijn op volgorde van datum, dus de rest van de tweets zijn ouder en hoeven ook niet bekeken te worden
-                break
-        return listTexts
+            return listTexts
+        except:
+            error = []
+            return error
 
 
